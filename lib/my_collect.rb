2c74cf914cb629eb8e24f7a5c8c301f903c71155
sasha walker <sashawalker31@gmail.com>
2020-01-31 03:39:45 +0000

    Done.

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,8 @@
-
-
+def my_collect(empty_array)
+  if my_collect(languages) do |language|
+    language. << yield (languages[i])
+  
+  elsif my_collect(students) do |student|
+    student.split(" ").first
+  end
+end
